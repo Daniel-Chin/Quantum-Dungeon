e30f477f8fa0b4b92c6ec35d93612266a3141c3a
Daniel Chin <daniel.chin@nyu.edu>
2022-07-24 18:46:46 +0400

    a

--- godot/scripts/Main.cs
+++ godot/scripts/Main.cs
@@ -29,24 +29,27 @@ public class Main : Node2D {
         int spawn_room_start = -2;
         int spawn_room_end = 3;
         for (int x = spawn_room_start; x < spawn_room_end; x ++) {
-            TheSeen[Tuple.Create(
-                x, 0         
-            )] = Tile.WALL;
-            TheSeen[Tuple.Create(
-                x, spawn_room_end - 1
-            )] = Tile.WALL;
             for (
                 int y = spawn_room_start; 
                 y < spawn_room_end; y ++
             ) {
                 int toPlace;
-                if (x == 0 || x == spawn_room_end - 1) {
+                if (
+                    x == spawn_room_start || 
+                    x == spawn_room_end - 1
+                ) {
                     toPlace = Tile.WALL;
                 } else {
                     toPlace = Tile.PATH;
                 }
                 TheSeen[Tuple.Create(x, y)] = toPlace;
             }
+            TheSeen[Tuple.Create(
+                x, spawn_room_start
+            )] = Tile.WALL;
+            TheSeen[Tuple.Create(
+                x, spawn_room_end - 1
+            )] = Tile.WALL;
         }
         TheSeen[Tuple.Create(0, spawn_room_start)] = Tile.DOOR;
 
