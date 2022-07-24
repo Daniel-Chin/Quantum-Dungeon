af6fbdd892800ea80a3088bd0b1e37471b8904be
Daniel Chin <daniel.chin@nyu.edu>
2022-07-24 17:41:00 +0400

    a

--- godot/scripts/Main.cs
+++ godot/scripts/Main.cs
@@ -4,13 +4,45 @@ using System.Collections.Generic;
 
 public class Main : Node2D {
     Dictionary<Tuple<int, int>, int> TheSeen;
+    Dictionary<Tuple<int, int>, int> TheOld;
+    Dictionary<Tuple<int, int>, int> TheFolk;
     Tuple<int, int> PlayerPos;
 
-    TileMap MyTileMap;
-    
+    TileMap SeenTileMap;
+    TileMap OldTileMap;
+    TileMap FolkTileMap;
+
     public override void _Ready() {
+        SeenTileMap = GetNode<TileMap> ("SeenTileMap");
+        OldTileMap  = GetNode<TileMap> ("OldTileMap");
+        FolkTileMap = GetNode<TileMap> ("FolkTileMap");
+        OldTileMap.Modulate = new Color(100, 100, 100);
+    }
+
+    public void Reset() {
         TheSeen = new Dictionary<Tuple<int, int>, int> ();
+        TheOld = new Dictionary<Tuple<int, int>, int> ();
+        TheFolk = new Dictionary<Tuple<int, int>, int> ();
         PlayerPos = Tuple.Create(0, 0);
-        MyTileMap = GetNode<TileMap> ("MyTileMap");
+    }
+
+    public void Draw() {
+        DrawTileMap(SeenTileMap, TheSeen);
+        DrawTileMap(OldTileMap, TheOld);
+        DrawTileMap(FolkTileMap, TheFolk);
+        var (x, y) = PlayerPos;
+        FolkTileMap.SetCell(x, y, Tile.PLAYER);
+    }
+    private void DrawTileMap(
+        TileMap tileMap, 
+        Dictionary<Tuple<int, int>, int> matrix
+    ) {
+        tileMap.Clear();
+        foreach (KeyValuePair<
+            Tuple<int, int>, int
+        > cell in matrix) {
+            var (x, y) = cell.Key;
+            tileMap.SetCell(x, y, cell.Value);
+        }
     }
 }
--- godot/scripts/Tile.cs
+++ godot/scripts/Tile.cs
@@ -2,9 +2,10 @@ using Godot;
 using System;
 
 public class Tile {
-    static readonly int WALL = 0;
-    static readonly int PATH = 1;
-    static readonly int DOOR = 2;
+    public static readonly int WALL = 0;
+    public static readonly int PATH = 1;
+    public static readonly int DOOR = 2;
+    public static readonly int PLAYER = 3;
 
     static Tile() {
     }
